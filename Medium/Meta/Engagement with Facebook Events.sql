4d9489a3b0a3c51b3d48a6a0d26e493a8d149307
hiepnguyenbg <hiepnguyen.career@gmail.com>
2025-10-22 09:46:21 +0200

    Update Engagement with Facebook Events.sql

--- Medium/Meta/Engagement with Facebook Events.sql
+++ Medium/Meta/Engagement with Facebook Events.sql
@@ -69,3 +69,74 @@ INSERT INTO fct_event_clicks (click_id, user_id, event_id, click_date) VALUES
 (8, 106, 207, '2024-03-15'),
 (9, 103, 208, '2024-03-20'),
 (10, 107, 209, '2024-03-25');
+
+
+
+-- As a Data Scientist on the Facebook Events Discovery team, you are tasked with analyzing user interaction with event 
+-- recommendations to enhance the relevance of these suggestions. Your goal is to identify which event categories receive 
+-- the most user clicks, determine if users are engaging with events in their preferred categories, and understand user 
+-- engagement patterns by analyzing click data. This analysis will help optimize recommendation algorithms to increase 
+-- user satisfaction and event attendance.
+
+
+-- Question 1: How many times did users click on event recommendations for each event category in March 2024? Show 
+-- the category name and the total clicks. 
+
+SELECT category_name, COUNT(*) AS total_clicks
+FROM fct_event_clicks fec
+JOIN dim_events de 
+ON fec.event_id = de.event_id
+WHERE click_date BETWEEN '2024-03-01' AND '2024-03-31'
+GROUP BY category_name
+ORDER BY total_clicks DESC;
+
+
+-- Question 2: For event clicks in March 2024, identify whether each user clicked on an event in their preferred category. 
+-- Return the user ID, event category, and a label indicating if it was a preferred category ('Yes' or 'No').
+
+
+
+ SELECT fec.user_id, category_name,
+   CASE WHEN category_name = preferred_category THEN 'Yes'
+   ELSE 'No'
+   END AS preferred_cat
+FROM fct_event_clicks fec
+JOIN dim_events de 
+  ON fec.event_id = de.event_id
+JOIN dim_users du
+   ON fec.user_id = du.user_id
+WHERE click_date BETWEEN '2024-03-01' AND '2024-03-31';
+
+
+-- Question 3: Generate a report that combines the user ID, their full name (first and last name), and the total clicks for events 
+-- they interacted with in March 2024. Sort the report by user ID in ascending order.
+
+
+SELECT du.user_id, CONCAT(du.first_name, ' ', du.last_name) AS full_name, 
+  COALESCE(COUNT(fec.click_id), 0) AS total_clicks
+FROM dim_users du
+LEFT JOIN fct_event_clicks fec
+ON du.user_id = fec.user_id
+AND click_date BETWEEN '2024-03-01' AND '2024-03-31'
+GROUP BY du.user_id, du.first_name, du.last_name
+ORDER BY du.user_id;
+
+
+-- Your analyses will help Meta understand user engagement with event recommendations by category, whether users are clicking on events 
+-- in their preferred categories, and how active users are overall. This insight is crucial for optimizing recommendation algorithms 
+-- to boost user satisfaction and event attendance.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
